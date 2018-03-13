9c6ae97f04b264b42f2854fb8973c52896d1d1af
George Theofilis <theofilis.g@gmail.com>
2018-03-13 13:21:53 +0200

    Add library assets

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Hash = require('../lib/binding/module.node');
+const nativeHasher = require('../lib/binding/module.node');
+class HashHelper {
+    constructor() {
+        this.nativeHasher = require('../lib/binding/module.node');
+        this.hash = (type, value) => {
+            return nativeHasher.hash(type, value);
+        };
+        this.md2 = (value) => this.hash('md2', value);
+        this.md5 = (value) => this.hash('md5', value);
+        this.sha256 = (value) => this.hash('sha256', value);
+        this.adler32 = (value) => this.hash('adler32', value);
+        this.tiger192 = (value) => this.hash('tiger192', value);
+        this.sha512 = (value) => this.hash('sha512', value);
+    }
+}
+exports.HashHelper = HashHelper;
+const Hash = new HashHelper();
 exports.Hash = Hash;
--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,5 +1,12 @@
-export interface IHashFunctions {
-    hash: (type: string, value: string) => string;
+export declare class HashHelper {
+    private nativeHasher;
+    hash: (type: string, value: string) => any;
+    md2: (value: string) => any;
+    md5: (value: string) => any;
+    sha256: (value: string) => any;
+    adler32: (value: string) => any;
+    tiger192: (value: string) => any;
+    sha512: (value: string) => any;
 }
-declare const Hash: IHashFunctions;
+declare const Hash: HashHelper;
 export { Hash };
