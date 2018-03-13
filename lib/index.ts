560635f7d3e89783e3a9c227aba9651c9b0c2dba
George Theofilis <theofilis.g@gmail.com>
2018-03-13 12:42:32 +0200

    Add some strongly methods

--- lib/index.ts
+++ lib/index.ts
@@ -1,8 +1,17 @@
-export interface IHashFunctions {
-  hash: (type: string, value: string) => string;
+const nativeHasher = require('../lib/binding/module.node');
+
+export class HashHelper {
+  hash = (type: string, value: string) => {
+    return nativeHasher.hash(type, value);
+  }
+
+  MD2 = (value: string) => this.hash('md2', value);
+  MD5 = (value: string) => this.hash('md5', value);
+  SHA256 = (value: string) => this.hash('sha256', value);
+  ADLER32 = (value: string) => this.hash('adler32', value);
 }
 
-const Hash: IHashFunctions = require('../lib/binding/module.node');
+const Hash = new HashHelper();
 
 export {
   Hash
--- test/Hash.spec.ts
+++ test/Hash.spec.ts
@@ -6,10 +6,18 @@ import { expect } from 'chai';
 const N = 1000;
 
 describe(`Check hash libraries`, () => {
-  it('MD5 tests', () => {
+  it('MD2 tests', () => {
+    expect(Hash.MD2('testing')).to.equal('fc134df10d6ecafceb5c75861d01b41f');
     expect(Hash.hash('md2', 'testing')).to.equal('fc134df10d6ecafceb5c75861d01b41f');
   });
+  it('MD5 tests', () => {
+    expect(Hash.MD5('testing')).to.equal('ae2b1fca515949e5d54fb22b8ed95575');
+  });
+  it('SHA256 tests', () => {
+    expect(Hash.SHA256('testing')).to.equal('cf80cd8aed482d5d1527d7dc72fceff84e6326592848447d2dc0b0e87dfc9a90');
+  });
   it('adler32 tests', () => {
+    expect(Hash.ADLER32('testing')).to.equal('0c1e02ff');
     expect(Hash.hash('adler32', 'testing')).to.equal('0c1e02ff');
   });
 
